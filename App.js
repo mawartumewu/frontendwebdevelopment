446b210a4e97d1b1dd76f390b36df0a15071fb38
mawartumewu <85202823+mawartumewu@users.noreply.github.com>
2021-12-07 14:21:04 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+// import Comment from './components/Comment';
+
+// const komentar = {
+//   text: "This is my commentar",
+//   date: new Date(),
+//   author: {
+//     avatarUrl: "https://via.placeholder.com/60",
+//     name: "John Doe"
+//   }
+// };
+
+// import HomePage from './pages/HomePage';
+// import EmployeePage from './pages/EmployeePage';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+
+// function App() {
+//   return(
+//     <Router>
+//       <div>
+//         {/* <Comment text={komentar.text} date={komentar.date} author={komentar.author} /> */}
+
+//         <Switch>
+//           <Route exact path="/" component={HomePage} />
+//           <Route path="/employee" component={EmployeePage} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// import ExampleStateHook from './components/ExampleStateHook';
+// import Counter from './components/Counter';
+
+// import Penghitung from './components/Penghitung';
+
+// import Todos from './components/todos/Todos';
+
+// function App() {
+//   return(
+//     <div>
+//       <Todos />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState, useMemo } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+//import Home from './pages/Home';
+//import About from './pages/About';
+import Todos from './components/todos/Todos';
+import { UserContext } from './UserContext';
+
+function App()  {
+  const [ user, setUser ] = useState('Guest');
+
+  const userValueProvider = useMemo(() => ({user, setUser}), [user, setUser]);
+
+  return(
+    <div>
+      <UserContext.Provider value={userValueProvider}>
+        <Routes>
+          <Route path="/" element={<Todos />}  />
+        </Routes>
+      </UserContext.Provider>
+    </div>
+  );
+}
+export default App;
